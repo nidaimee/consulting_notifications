879ee0b6faf19c770a79856e86334c623a538bdd
dutrarg1@gmail.com <dutrarg1@gmail.com>
2025-09-27 14:41:21 -0300

    adjusted in file of pdf

--- app/jobs/generate_pdf_job.rb
+++ app/jobs/generate_pdf_job.rb
@@ -1,6 +1,8 @@
 class GeneratePdfJob < ApplicationJob
   def perform(client, html, user_email)
-    pdf = WickedPdf.new.pdf_from_string(html, ...)
+    # Explicitly pass any required options instead of using `...`
+    pdf_options = { encoding: "UTF-8" } # Example options for WickedPdf
+    pdf = WickedPdf.new.pdf_from_string(html, pdf_options)
     UserMailer.pdf_ready(client, pdf, user_email).deliver_now
   end
 end
