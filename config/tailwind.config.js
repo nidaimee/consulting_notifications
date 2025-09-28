edc1cd0a6e123b943b7f15941512ee09f41d153f
dutrarg1@gmail.com <dutrarg1@gmail.com>
2025-09-27 22:06:08 -0300

    adjusted few files to deploy

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,8 +5,7 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}',
-    './app/components/**/*.{erb,rb}' // Caso use view components
+    './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
     extend: {
@@ -16,8 +15,9 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
+    // Remover temporariamente os plugins que podem causar problema
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/aspect-ratio'),
+    // require('@tailwindcss/typography'),
   ]
 }
